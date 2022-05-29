92cc1a861afea709602549897969f398648e0640
unbxd <anwartauseef37@gmail.com>
2022-05-29 16:52:32 +0530

    DONE: attachment:final change_encoding correction

--- networking/mailing_client.py
+++ networking/mailing_client.py
@@ -1,3 +1,4 @@
+from email.mime import text
 import smtplib
 from email import encoders
 from email.mime.text import MIMEText
@@ -22,5 +23,19 @@ with open(r"message.txt","r") as m:
     message = m.read()
     
 msg.attach(MIMEText(message,"plain"))
+
+filename = "image.jpeg"
+attachment = open(filename,"rb")
+
+p = MIMEBase("application","octet-stream")
+p.set_payload(attachment.read())
+
+encoders.encode_base64(p)
+p.add_header("Content-Disposition",f'attachment;filename={filename}')
+
+msg.attach(p)
+
+text = msg.as_string()
+server.sendmail("anwartauseef37@gmail.com","1993taz@gmail.com",text)
 
  
